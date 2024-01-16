a6fe8f8928da10c389565e34541cdc003fa8a274
djuradjb <bubalodjuradj@gmail.com>
2024-01-16 22:04:07 +0100

    Dodata registracija korisnika, sa upisom u bazu podataka

--- flaskr/models/FlaskForms.py
+++ flaskr/models/FlaskForms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField
+from wtforms import EmailField, PasswordField, SubmitField, StringField
 from wtforms.validators import Email, DataRequired, Length
 
 class LoginForm(FlaskForm):
     email = EmailField(validators=[Email(),DataRequired(),Length(max=40)])
     lozinka = PasswordField(validators=[DataRequired(),Length(max=40)])
     submit = SubmitField(label='Uloguj se')
+
+class RegisterForm(FlaskForm):
+    ime=StringField(validators=[Length(min=2, max=20), DataRequired()])
+    prezime=StringField(validators=[Length(min=2, max=20), DataRequired()])
+    adresa=StringField(validators=[DataRequired(),Length(min=3, max=40)])
+    grad=StringField(validators=[DataRequired(),Length(min=2, max=20)])
+    drzava = StringField(validators=[DataRequired(),Length(min=4, max=20)])
+    telefon = StringField(validators=[DataRequired(),Length(min=8, max=10)])
+    email = EmailField(validators=[Email(),DataRequired(),Length(max=40)])
+    lozinka = PasswordField(validators=[DataRequired(),Length(min=8,max=40)])
+    submit = SubmitField(label='Otvori nalog')
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,17 +1,17 @@
-from flaskr import app
+from flaskr import app, db
 from flask import render_template, request, redirect, url_for
-import flask_login
+from flask_login import LoginManager, login_user, login_required
 from flaskr.models.Korisnici import Korisnici
-from flaskr.models.FlaskForms import LoginForm
+from flaskr.models.FlaskForms import LoginForm, RegisterForm
 
 #flask login
-login_manager = flask_login.LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
 @login_manager.user_loader
 def user_loader(id):
     return Korisnici.query.get(int(id))
 
-
+#login (homepage)
 @app.route('/', methods=['GET','POST'])  
 @app.route("/auth/login", methods=['GET','POST'])
 def login():
@@ -19,7 +19,6 @@ def login():
     if form.validate_on_submit():
         email = form.email.data
         lozinka = form.lozinka.data
-        print(email)
         if(email == "" or email.isspace()
            or  lozinka == "" or lozinka.isspace()):
             return redirect(url_for('login'))
@@ -27,20 +26,44 @@ def login():
         
         if (not u or u.lozinka!=lozinka):
             return redirect(url_for('login'))
-        flask_login.login_user(u)
+        login_user(u)
         return redirect(url_for('portfolio'))
     return render_template('auth/login.html',form=form)
 
 #register
 @app.route('/auth/register', methods=['GET','POST'])
 def register():
-    return render_template('auth/register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        ime = form.ime.data
+        prezime = form.prezime.data
+        adresa = form.adresa.data
+        grad = form.grad.data
+        drzava = form.drzava.data
+        telefon = form.telefon.data
+        email = form.email.data
+        lozinka = form.lozinka.data
+        if (ime == "" or ime.isspace()
+            or prezime == "" or prezime.isspace()
+            or adresa == "" or adresa.isspace()
+            or grad == "" or grad.isspace()
+            or drzava == "" or drzava.isspace()
+            or telefon == "" or telefon.isspace()
+            or email == "" or email.isspace()
+            or lozinka == "" or lozinka.isspace()):
+            return redirect(url_for('register'))
+        u = Korisnici(ime=ime, prezime= prezime, adresa=adresa, grad=grad, drzava=drzava, telefon=telefon, email=email, lozinka=lozinka)
+        db.session.add(u)
+        db.session.commit()
+        login_user(u)
+        return redirect(url_for('portfolio'))
+    return render_template('auth/register.html', form=form)
 
     
 
 #Portfolio nakon login-a
 @app.route('/portfolio', methods=['GET','POST'])
-@flask_login.login_required
+@login_required
 def portfolio():
     return render_template('portfolio.html')    
         
